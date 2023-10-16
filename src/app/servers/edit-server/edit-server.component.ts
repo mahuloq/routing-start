dfc3ffb014ecf37620cdc2af633fc0910cda0cc4
Matt Tucker <arcanemahuloq@gmail.com>
2023-10-15 22:31:31 -0500

    Implemented AuthGuard, and baseic login service

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
 import { ServersService } from "../servers.service";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 import { query } from "@angular/animations";
 
 @Component({
@@ -14,10 +14,12 @@ export class EditServerComponent implements OnInit {
   serverName = "";
   serverStatus = "";
   allowEdit = false;
+  changesSaved = false;
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -38,5 +40,7 @@ export class EditServerComponent implements OnInit {
       name: this.serverName,
       status: this.serverStatus,
     });
+    this.changesSaved = true;
+    this.router.navigate(["../"], { relativeTo: this.route });
   }
 }
